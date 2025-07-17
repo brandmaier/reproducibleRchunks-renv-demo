4152d597eaa3aca15d1f2ff94dee417f0d546084
brandmaier <brandmaier@mpib-berlin.mpg.de>
2025-07-17 12:42:28 +0200

    added local isReproducible file

---
+++ isReproducible.R
@@ -0,0 +1,35 @@
+#' @export
+#' @title Test reproducibility of an R Markdown file
+#'
+#' @param filename Character. An R Markdown file to check for reproducibility
+#' @param resetOptions Boolean. Should all package options be reset to defaults? TRUE by default. This avoids problems if multiple checks on multiple documents with varying options are made in a row
+#' @param \ldots Optional arguments passed down to \code{rmarkdown::render()}
+#'
+isReproducible <- function(filename, resetOptions = TRUE, run_pandoc = FALSE,
+                           ...)
+{
+  if (!file.exists(filename)) stop("File does not exist")
+  if (!endsWith(tolower(filename),"rmd")) warning("Possibly not an Rmd file")
+
+  if (resetOptions) {
+    ids <- (startsWith(names(options()),"reproducibleRchunks."))
+    opnames <- names(options())[ids]
+    for (name in opnames)    options(structure(list(NULL), names = name))
+  }
+
+  .reset()
+
+  rmarkdown::render(input = filename,
+                    quiet = TRUE,
+                    runtime = "static",
+                    run_pandoc = run_pandoc,
+                    ...)
+
+  if (nrow(get_reproducibility_summary()) == 0) {
+    message("No reproducibility information found! Could not evaluate reproducibility status.")
+    return(NA)
+  }
+
+  return(all(get_reproducibility_summary()$Success))
+
+}
